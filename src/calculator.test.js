157f33285f20e92ee70735ee4e488901b8989df6
Hafsteinn <hafsteinn24@gmail.com>
2018-10-10 18:28:38 +0000

    implemented add for multiple numbers + refactoring

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -8,6 +8,10 @@ it("should return when only one number is in the string", () => {
   expect(add("5")).toBe(5);
 });
 
-it("should return sum of two given numbers ", () => {
+it("should return sum of two given numbers", () => {
   expect(add("1,2")).toBe(3);
+});
+
+it("should summarize multiple numbers", () => {
+  expect(add("1,2,3")).toBe(6);
 });
